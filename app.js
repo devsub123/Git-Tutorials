9a998c40681b0e2230ad7853b59dc7de440b00af
Devansh <devansh.subnani05@gmail.com>
2022-04-29 21:10:41 +0530

    first commit

--- app.js
+++ app.js
@@ -1,44 +1,56 @@
-// const userInput = prompt("Enter something")
+// let random = Math.random()
+// if(random<0.5){
+//     console.log("YOUR NUMBER IS LESS THAN 0.5")
+// }
 
-// if(userInput){
-//     console.log("TRUTHY!")
+// else{
+//     console.log("YOUR NUMBER IS GREATER(OR EQUAL) THAN 0.5")
+// }
+// console.log(random)
+
+
+// const dayOfWeek = prompt('ENTER A DAY').toLowerCase()
+
+// if(dayOfWeek === 'monday'){
+//     console.log("UGHHH I HATE MONDAYS");
+// } else if(dayOfWeek === 'saturday'){
+//     console.log("YAY I LOVE SATURDAYS")
+// } else if(dayOfWeek === 'friday'){
+//     console.log("FRIDAYS ARE DECENT ESPECIALLY AFTER WORK")
 // } else{
-//     console.log("FALSY")
+//     console.log("MEH")
 // }
 
-// if(''){
-//     console.log("TRUTHY!")
+
+// 0-5 FREE
+// 5-10 CHILD $10
+// 10-65 ADULT $20
+// 65+ $10
+
+// const age = 3
+
+// if(age<5){
+//     console.log("You are a baby.You get in for free")
+// } else if(age <10){
+//     console.log("You are a child.You pay $10") 
+// } else if(age <65){
+//     console.log("You are an adult.You pay $20") 
 // } else{
-//     console.log("FALSY!")
+//     console.log("You are a senior.You py $10")
 // }
 
 
-// const password = prompt("Enter your password")
-// if(password.length > 6 && password.indexOf(' ') === -1){
-//     console.log("VALID PASSWORD!")
-// } else {
-//     console.log("INCORRECT FORMAT FOR PASSWORD!")
-// }
+
+const password = prompt('please enter a new password')
+
+if (password.length >= 6) {
+    if (password.indexOf(' ') === -1) {
+        console.log("Valid password")
+    } else {
+        console.log("Password cannot contain spaces!")
+    }
+} else {
+    console.log("PASSWORD TOO SHORT!Must be 6+ characters")
+}
 
 
-// const age = 3;
-// if((age >= 0 && age < 5) || age >= 65){
-//     console.log("FREE")
-// } else if(age >=5 && age < 10){
-//     console.log("$10")
-// } else if(age >=10 && age < 65){
-//     console.log("$20")
-// } else {
-//     console.log("INVALID AGE!")
-// }
-
-
-// let firstName = prompt("enter your first name")
-// if(!firstName){
-//     firstName = prompt("TRY AGAIN!")
-// }
-
-const age = 8;
-if(!(age >= 0 && age < 5 || age >= 65)){
-    console.log("YOU ARE NOT A BABY OR A SENIOR")
-} 
