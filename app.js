e8fe46346b53d0957eb6e4f285e3cb9f88cb4d9e
Devansh <devansh.subnani05@gmail.com>
2022-04-29 20:23:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+// const userInput = prompt("Enter something")
+
+// if(userInput){
+//     console.log("TRUTHY!")
+// } else{
+//     console.log("FALSY")
+// }
+
+// if(''){
+//     console.log("TRUTHY!")
+// } else{
+//     console.log("FALSY!")
+// }
+
+
+// const password = prompt("Enter your password")
+// if(password.length > 6 && password.indexOf(' ') === -1){
+//     console.log("VALID PASSWORD!")
+// } else {
+//     console.log("INCORRECT FORMAT FOR PASSWORD!")
+// }
+
+
+// const age = 3;
+// if((age >= 0 && age < 5) || age >= 65){
+//     console.log("FREE")
+// } else if(age >=5 && age < 10){
+//     console.log("$10")
+// } else if(age >=10 && age < 65){
+//     console.log("$20")
+// } else {
+//     console.log("INVALID AGE!")
+// }
+
+
+// let firstName = prompt("enter your first name")
+// if(!firstName){
+//     firstName = prompt("TRY AGAIN!")
+// }
+
+const age = 8;
+if(!(age >= 0 && age < 5 || age >= 65)){
+    console.log("YOU ARE NOT A BABY OR A SENIOR")
+} 
